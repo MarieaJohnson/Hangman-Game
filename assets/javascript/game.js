55690626301b1c54ce7107ce20099c5daf08d920
Mariea Johnson <marieajohnson@Marieas-MacBook-Pro.local>
2018-02-17 20:30:00 -0500

    revised files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,39 @@
 // Hangman-Game
 console.log("Javascript is connected");
 
+  // src="http://code.jquery.com/jquery-3.3.1.js"
+  // integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  // crossorigin="anonymous"
+
+  // $(document).ready(function(){
+
+document.write("Press any key to get started!");  
+
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
 var puzzles = ["horse", "cat", "dog", "chicken", "cow", "sheep"];
-var guess = [];
+var userGuess;
+//var guesses = [];
 var noOfGuesses = 0;
 var noOfLetters = ["_"];
 var chosenLetters = [];
-// Track wins/losses - Restart counter after 5 wins or losses
-var wins = 0;
-var losses = 0;
+
+//Create letter buttons
+// for (var m=0; m<options.length; m++) {
+//   var m = $("<button>");
+//   m.addClass("letter-button letter letter-button-color");
+//   m.attr("data-let", letters[m]);
+//   $("#buttons").apprend(m);
+// }
+
+// $(".letter-button").on("click",function(){
+//   var userGuess = $('<div class="chosenLetters">').text($(this).data("let"));
+
+//   $("#display").append(newPuzzle);
+// });
+
+// $("#new-game").on("click", function(){
+//   $("#display").empty();
+// });
 
 // Choose random word from puzzles array
 var newPuzzle = puzzles[Math.floor(Math.random() * puzzles.length)];
@@ -19,68 +43,71 @@ var dashedWord = [];
 
 // Create placeholders for number of letters in puzzle
 for (var i = 0; i < newPuzzle.length; i++) {
-  dashedWord.push("_");
-}
+      dashedWord.push("_");
+    }
 console.log(dashedWord)
-// Displaying dashes for puzzle
+
+//Display placeholder lines for puzzle
 console.log(dashedWord.join(" "));
 document.getElementById("puzzleGuess").innerHTML = dashedWord.join(" ");
 
-// Display puzzle word with letter placeholders the # of letters in puzzle  
-// function logArray(arr) {
-//   for (i = 0; i < puzzle.length; i++) {
-//     console.log(puzzle[i]);
-//     document.write(noOfLetters[i]
-//         }
-// }
-
-// for (let index = 0; index < array.length; index++) {
-// const element = array[index];
-// }
-
-while (chosenLetters > 0) {
-
-  document.onkeyup = function () {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-console.log(userGuess letter)
-    document.getElementById(userGuess);
-
-    // Compare userGuess to letters in puzzle
-
+// User presses button of letter
+    // document.addEventListener("keydown", function(event){
+    
+// Change input to lowercase    
+    // var userGuess = String.fromCharCode(event.keyCode).toLowerCase()
+      //console.log(userGuess)
+    
+    // document.getElementById(userGuess.join(" "));
     // If userGuess = Letter in Puzzle
-    if (userGuess === "a letter in the puzzle");
+    // if (userGuess === "a letter in the puzzle");
 
-    // If correct show userGuess placement in puzzle
-    document.getElementById(userGuess);
+// If correct show userGuess placement in puzzle
+    //document.getElementById(userGuess);
 
+// If userGuess <> letter in puzzle.
+// Show wrong letters chosen at the bottom of the page.
+    // alert("Please select another letter.")
 
+// Compare userGuess to letters in puzzle
+  // if (userGuess === null){
 
-    // If userGuess <> letter in puzzle.
-    //  Show wrong letters chosen at the bottom of the page.
-    alert("Please select another letter.")
+  // }
+  // else if (userGuess.length !== 1) {
+  // }
+  // else {
 
-    // If user completes puzzle
-    wins++;
-    ("Wins: " + wins);
+// Update the blanks with the guess 
+//   for (var k = 0; k < newPuzzle.length; k++) {
+//     if (newPuzzle[k] === userGuess) {
+//       dashedWord[k] = userGuess;
+//       remainingLetters--;
+//     }
+//   }
+// }
+// }
 
-    // If user chooses to autocomplete puzzle
-    losses++;
-    ("Losses: " + losses);
+// If user completes puzzle
+  //   wins++;
+  //   ("Wins: " + wins);
 
-    // Show number of remaining guesses
-    var noOfGuesses = document.getElementById("guessesLeft");
+  //   // If user does not solve puzzle
+  //   losses++;
+  //   ("Losses: " + losses);
 
-    comments = function () {
-      noOfGuesses.innerHTML = "You have " + noOfGuesses + " guesses left.";
-      if (noOfGuesses < 1) {
-        noOfGuesses.innerHTML = "Game Over";
-      }
-      for (var i = 0; i < puzzle.length; i++) {
-        if (counter + space === puzzle.length) {
-          noOfGuesses.innerHTML = "You Win!";
-        }
-      }
-    }
-  }
-}
+  // Show number of remaining guesses
+    // var noOfGuesses = document.getElementById("guessesLeft");
+    // });
+
+    // comments = function () {
+    //     noOfGuesses.innerHTML = "You have " + noOfGuesses + " guesses left.";
+
+    //   if (noOfGuesses < 1) {
+    //       noOfGuesses.innerHTML = "Game Over";
+    //     }
+    //       for (var l = 0; l < puzzle.length; l++){
+    //         if (counter + space === puzzle.length) {
+    //           noOfGuesses.innerHTML = "You Win!";
+    //         }
+    //     }
 // Once puzzle is solved, reveal new puzzle
